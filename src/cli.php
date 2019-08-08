e071936626784cb876617f13757c2235a66b2f4d
LubaRo <cnlmode@gmail.com>
2019-08-08 21:35:42 +0400

    add greeting

---
+++ src/cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function \cli\line as line;
+use function \cli\prompt as prompt;
+
+function run()
+{
+    $name = prompt('May I have your name?');
+
+    line("Hello, %s!", $name);
+}
