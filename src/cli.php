a9a0c3bffb022d5d49ef76f6a82fcfae1d2a2d59
LubaRo <cnlmode@gmail.com>
2019-10-17 20:57:20 +0400

    fix build

--- src/cli.php
+++ src/cli.php
@@ -2,21 +2,21 @@
 
 namespace BrainGames\Cli;
 
-use function \cli\line as line;
-use function \cli\prompt as prompt;
+use function cli\line as line;
+use function cli\prompt as prompt;
 
 function get_user_name()
 {
     $name = prompt('May I have your name?');
 
-    line("Hello, %s!", $name);
+    line("Hello, %s!\n", $name);
 
     return $name;
 }
 
 function greet()
 {
-    line("Welcome to the Brain Games!\n\n");
+    line("\nWelcome to the Brain Games!");
 
     return;
 }
--- src/games/even.php
+++ src/games/even.php
@@ -2,9 +2,9 @@
 
 namespace BrainGames\EvenGame;
 
-use function \BrainGames\Engine\play_game,
-             \BrainGames\Engine\fn_format_answer,
-             \BrainGames\Engine\fn_get_random_number;
+use function BrainGames\Engine\play_game;
+use function BrainGames\Engine\fn_format_answer;
+use function BrainGames\Engine\fn_get_random_number;
 
 function even_game_play()
 {
@@ -12,12 +12,14 @@ function even_game_play()
         function () {
             $random_num = fn_get_random_number();
 
-            $question = "Question: {$random_num}";
+            $question = "{$random_num}";
             $correct_answer = fn_get_correct_answer($random_num);
 
             return array($question, $correct_answer);
         }
     );
+
+    return true;
 }
 
 function fn_is_even($number)
