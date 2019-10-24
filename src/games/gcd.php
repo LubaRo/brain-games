ae3e119d2887300bf2fcaf79113947cd7f1eee98
LubaRo <cnlmode@gmail.com>
2019-10-24 21:50:12 +0400

    add gcd game

--- src/games/gcd.php
+++ src/games/gcd.php
@@ -1,21 +1,34 @@
 <?php
 
-namespace BrainGames\EvenGame;
+namespace BrainGames\GcdGame;
 
 use function BrainGames\Engine\play_game;
 use function BrainGames\Engine\fn_get_random_number;
 
-function even_game_play()
+function gcd_game_run()
 {
     play_game(
         function () {
-            list($num1, $num2, $math_sign) = fn_get_expression_components();
-            $expression = "{$num1} {$math_sign} {$num2}";
+            $num1 = fn_get_random_number(1, 100);
+            $num2 = fn_get_random_number(1, 100);
 
-            $question = "Question: {$expression}";
-            $correct_answer = fn_get_correct_answer($num1, $num2, $math_sign);
+            $question = "{$num1} {$num2}";
+            $correct_answer = fn_get_gsd($num1, $num2);
 
             return array($question, (string) $correct_answer);
         }
     );
+}
+
+function fn_get_gsd($num1, $num2)
+{
+    while ($num1 != 0 && $num2 != 0) {
+        if ($num1 > $num2) {
+            $num1 = $num1 % $num2;
+        } else {
+            $num2 = $num2 % $num1;
+        }
+    }
+
+    return $num1 + $num2;
 }
