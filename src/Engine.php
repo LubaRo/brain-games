aff7b04863e939e5ac8068861a593fa327771aef
LubaRo <cnlmode@gmail.com>
2019-11-12 21:49:51 +0400

    remove excess variable

--- src/Engine.php
+++ src/Engine.php
@@ -14,12 +14,11 @@ function playGame(string $description, callable $getGameData)
     line($description);
     line();
 
-    $correctAnswers = 0;
     $userName = prompt('May I have your name?');
 
     line("Hello, %s!\n", $userName);
 
-    for ($i = ATTEMPTS_QTY; $i > 0; $i = $i - 1) {
+    for ($i = 0; $i < ATTEMPTS_QTY; $i = $i + 1) {
         list($question, $correctAnswer) = $getGameData();
 
         $userAnswer = prompt("Question: " . $question);
@@ -28,7 +27,6 @@ function playGame(string $description, callable $getGameData)
         $isCorrect = $correctAnswer === $userAnswer ? true : false;
 
         if ($isCorrect === true) {
-            $correctAnswers += 1;
             line("Correct!\n");
         } else {
             line("'%s' is wrong answer ;(. Correct answer was '%s'.\n\n" .
