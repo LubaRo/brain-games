b4684d6e9af7e5807bf430c51e35cd2ffb3f0f92
LubaRo <cnlmode@gmail.com>
2019-11-14 21:24:34 +0400

    fix bug

--- src/Games/Calculator.php
+++ src/Games/Calculator.php
@@ -12,7 +12,7 @@ function calculatorGameRun()
 {
     $getGameData = function () {
         $num1 = rand(1, 10);
-        $num2 = rand();
+        $num2 = rand(1, 10);
 
         $signKey = rand(0, sizeof(MATH_SIGNS) - 1);
         $mathSign = MATH_SIGNS[$signKey];
