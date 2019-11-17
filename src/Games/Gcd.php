8347bda8be06f5a57409e375b18228d58c1db42f
LubaRo <cnlmode@gmail.com>
2019-11-17 19:47:38 +0400

    remove usless braces

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -12,7 +12,7 @@ function gcdGameRun()
         $num1 = rand(1, 100);
         $num2 = rand(1, 100);
 
-        $question = "{$num1} {$num2}";
+        $question = "$num1 $num2";
         $correctAnswer = getGcd($num1, $num2);
 
         return array($question, (string) $correctAnswer);
